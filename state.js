bb0ff93ab5d9a96525f3ac75e4758cf60da3fa25
my9941652-create <my9941652@gmail.com>
2026-06-30 14:04:58 +0530

    feat: save transaction log records databases to LocalStorage

---
+++ state.js
@@ -0,0 +1,26 @@
+let transactions = [];
+const STORAGE_KEY = 'spendwise_transactions';
+
+export function getTransactions() {
+  return transactions;
+}
+
+function saveTransactions() {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(transactions));
+}
+
+export function addTransaction(transaction) {
+  transactions.push(transaction);
+  saveTransactions();
+}
+
+export function deleteTransaction(id) {
+  transactions = transactions.filter(t => t.id !== id);
+  saveTransactions();
+}
+
+export function initTransactions() {
+  const cached = localStorage.getItem(STORAGE_KEY);
+  if (cached) {
+    try {
+      transactions = JSON.parse(cached);
